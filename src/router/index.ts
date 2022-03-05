d038d451838c0c80cfcc4f5dc1cee6152cea6882
nikolay <tneomichi@gmail.com>
2022-03-05 11:50:37 +0500

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
+import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-
+  {
+    meta: { role: 0, title: 'main', showInMenu: true },
+    path: '/',
+    name: 'home',
+    component: Home
+  }
 ]
-const history=createWebHistory()
+const history = createWebHistory()
 const router = createRouter({
   history, routes
 })
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="center" >
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+    <h1 class="center">home</h1>
+  </div>
+</template>
+
+
+<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+</style>
