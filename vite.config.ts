b9737c0d5335e4af29870b75fd5fdbc890868663
nikolay <tneomichi@gmail.com>
2022-03-05 11:17:09 +0500

    fix
    создал представление и добавил  роут  , поправил vite.config для публикации

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ["vue", "vue-router"],
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    open: true,  
+  },
 })
