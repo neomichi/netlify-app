59e733d8284c7b9bab87b33a462a9e998a2e1249
nikolay <tneomichi@gmail.com>
2022-03-05 11:07:20 +0500

    route
    add vue-routing  and register in main

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+
+const routes: Array<RouteRecordRaw> = [
+
+]
+const history=createWebHistory()
+const router = createRouter({
+  history, routes
+})
+
+export default router
