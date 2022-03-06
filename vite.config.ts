1daf3b80209f40da56d4b63bfd790b52cebb4fdc
nikolay <tneomichi@gmail.com>
2022-03-06 11:18:33 +0500

    fix types node

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path  from "path";
+import * as fs from "fs";
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   optimizeDeps: {
     include: ["vue", "vue-router"],
+  },
+  // css: {
+  //   postcss: {
+  //     plugins: [
+  //       autoprefixer()
+  //     ]
+  //   }
+  // },
+
+  
+  resolve: { 
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+    },
   },
   server: {
     host: "0.0.0.0",
