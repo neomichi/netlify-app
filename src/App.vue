639db4e3b89164de503a04d834592330887cf7f8
nikolay <tneomichi@gmail.com>
2022-03-06 11:16:46 +0500

    создание scss стилей + подключение в app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
-</script>
+
+
+
+
 
 <template>
-<div>
- 
-
-
-     <router-view/>
-    
-   </div>    
+<div>  
+   <router-view class="main" />    
+ </div>    
 </template>
+
+<script lang="ts">
+import "./assets/scss/main.scss"
+</script>
 
 
 
